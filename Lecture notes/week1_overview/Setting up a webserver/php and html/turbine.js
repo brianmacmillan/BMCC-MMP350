22f65fa36131623c8a92bb39d658e7df11b07b63
brianmacmillan <brmacmillan@gmail.com>
2017-01-30 17:17:10 -0500

    third draft of syllabus with more examples

--- Lecture notes/week1_overview/Setting up a webserver/php and html/turbine.js
+++
@@ -1,9 +0,0 @@
-function hide(e){
-	e.classList.add("invisible");
-}
-function show(e){
-	e.classList.remove("invisible");
-}
-function showHTML(e){
-	alert("The innerHTML property of this element is "+e.innerHTML);
-}
